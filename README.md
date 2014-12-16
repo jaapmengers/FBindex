4f01d6a8de05e5f1e18b8e5e3807eb21cc18c8e2
Jaap Mengers <jaapm@q42.nl>
2014-12-16 22:08:25 +0100

    Added mapping and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 FBindex
 =======
+
+To use, add an access_token.js file to the root of the project that contains an access token that you can generate at https://developers.facebook.com/tools/explorer/
+
+It should look like this:
+
+```
+exports.access_token = '[yourtoken]';
+```
--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var http = require('http'),
   us = require('underscore');
   at = require('./access_token.js');
 
-var url = "https://graph.facebook.com/me/links?method=GET&format=json&suppress_http_code=1&access_token=" + at.access_token;
+var url = 'https://graph.facebook.com/me/links?method=GET&format=json&suppress_http_code=1&access_token=' + at.access_token;
 
 var getPosts = function(url){
 
@@ -18,6 +18,7 @@ var getPosts = function(url){
 
 		res.on('end', function(){
 			var response = JSON.parse(body);
+			console.log(response);
 			console.log('Got response: ', response.data.length);			
 
 			us.each(response.data, function(it){
@@ -39,7 +40,7 @@ var save = function(it){
 
 	var options = {
 	  host: 'localhost',
-	  port: 9200,
+	  port: 9205,
 	  path: '/links/link/' + it.id,
 	  method: 'PUT'
 	};
